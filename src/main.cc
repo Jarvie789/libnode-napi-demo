df7e5e821a634464107c7ce51490af510549e800
Jarvie789 <jarvie789@gmail.com>
2024-09-29 09:46:53 +0800

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -9,7 +9,7 @@ Napi::Boolean _isWindow(const Napi::CallbackInfo& info) {
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::Boolean::New(env, "isWindow"),
+  exports.Set(Napi::String::New(env, "isWindow"),
               Napi::Function::New(env, _isWindow));
 
   return exports;
